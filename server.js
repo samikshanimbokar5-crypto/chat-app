1956dd90c7d6d66d7f4fa6952fc61b0c5e667e19
samikshanimbokar5 <samikshanimbokar5@gmail.com>
2026-06-19 10:58:44 -0400

    fix cors for deployment

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
-const userRoutes = require("./routes/userRoutes");
+
 require("dotenv").config();
 
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const { protect } = require("./middleware/authMiddleware");
 const { initSocket } = require("./socket");
 
+
 const app = express();
 
 
+// CORS CONFIG
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://chat-app-qgkc.onrender.com"
     ],
     methods: [
       "GET",
@@ -30,55 +34,92 @@ app.use(
   })
 );
 
+
 app.use(express.json());
 
 
-// Connect Database
+
+// Database
 connectDB();
 
 
+
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes);
+
+app.use(
+  "/api/auth",
+  authRoutes
+);
+
+
+app.use(
+  "/api/messages",
+  messageRoutes
+);
+
+
+app.use(
+  "/api/users",
+  userRoutes
+);
+
 
 
 // Home Route
-app.get("/", (req, res) => {
+
+app.get("/", (req,res)=>{
 
   res.send("Server Running");
 
 });
 
 
+
+
 // Protected Route
-app.get("/api/protected", protect, (req, res) => {
 
-  res.json({
+app.get(
+  "/api/protected",
+  protect,
+  (req,res)=>{
 
-    message: "Protected Route Accessed",
+    res.json({
 
-    user: req.user,
+      message:"Protected Route Accessed",
 
-  });
+      user:req.user
 
-});
+    });
+
+  }
+);
 
 
-// Create HTTP Server
+
+
+// HTTP Server
+
 const server = http.createServer(app);
 
 
-// Initialize Socket.IO
+
+
+// Socket.IO
+
 initSocket(server);
 
 
+
+
 // Start Server
+
 const PORT = process.env.PORT || 8000;
 
 
-server.listen(PORT, () => {
+server.listen(PORT,()=>{
 
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`
+  );
 
 });
